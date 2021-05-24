50423f459c5face06472a933067c181e8a2d9f45
Jonas Teuwen <jonasteuwen@gmail.com>
2021-05-24 21:04:47 +0200

    Renaming variables

---
+++ dlup/dataset/wsi_dataset.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+# Copyright (c) dlup contributors
+import logging
+
+class WsiTileDataset:
+    def __init__(self, wsi_filename):
+        self.logger = logging.getLogger(type(self).__name__)
+        self.wsi_filename = wsi_filename
+
