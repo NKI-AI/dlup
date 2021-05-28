4f1cd217f0134e2bf736dc4705d0fa3f36d25118
Jonas Teuwen <jonasteuwen@gmail.com>
2021-05-28 18:57:41 +0200

    Expanded tile metadata with filename

--- dlup/dataset/wsi_dataset.py
+++ dlup/dataset/wsi_dataset.py
@@ -2,8 +2,8 @@
 # Copyright (c) dlup contributors
 import logging
 
+
 class WsiTileDataset:
     def __init__(self, wsi_filename):
         self.logger = logging.getLogger(type(self).__name__)
         self.wsi_filename = wsi_filename
-
