58558cec257fac15fbb06031c84626a2e5dd2504
Leonardo Romor <leonardo.romor@gmail.com>
2021-06-28 15:21:26 +0200

    Improved SlideImage initialization exception messages.

--- dlup/_exceptions.py
+++ dlup/_exceptions.py
@@ -1,7 +1,11 @@
 # coding=utf-8
+
+
 class DlupError(Exception):
     pass
 
 
 class DlupUnsupportedSlideError(DlupError):
-    pass
+    def __init__(self, msg, *args, identifier=None, **kwargs):
+        msg = f"slide '{identifier}': " + msg if identifier is None else msg
+        super().__init__(self, msg, *args, **kwargs)
