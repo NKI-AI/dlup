1bea0d8359c034b5ae14b645668b4b6250eead78
Leonardo Romor <leonardo.romor@gmail.com>
2021-06-28 16:07:18 +0200

    Fix opposite message.

--- dlup/_exceptions.py
+++ dlup/_exceptions.py
@@ -1,11 +1,14 @@
 # coding=utf-8
 
 
+from typing import Optional
+
+
 class DlupError(Exception):
     pass
 
 
 class DlupUnsupportedSlideError(DlupError):
-    def __init__(self, msg, *args, identifier=None, **kwargs):
-        msg = f"slide '{identifier}': " + msg if identifier is None else msg
-        super().__init__(self, msg, *args, **kwargs)
+    def __init__(self, msg: str, identifier: Optional[str] = None):
+        msg = msg if identifier is None else f"slide '{identifier}': " + msg
+        super().__init__(self, msg)
