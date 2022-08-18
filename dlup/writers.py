1cf1fd40841c78e0985d081036f62166aea416fd
Jonas Teuwen <j.teuwen@nki.nl>
2022-08-18 18:42:24 +0200

    Swap axis correctly

--- dlup/writers.py
+++ dlup/writers.py
@@ -92,7 +92,7 @@ class TifffileImageWriter(ImageWriter):
         """
         self._filename = pathlib.Path(filename)
         self._tile_size = tile_size
-        self._size = (*size[::-1], 1) if len(size) == 2 else size[::-1]
+        self._size = (*size[::-1], 1) if len(size) == 2 else (size[1], size[0], size[2])
         self._mpp: Tuple[float, float] = (mpp, mpp) if isinstance(mpp, float) else mpp
 
         if not compression:
