0e8369422be8c1113bb5b4f7556843d15c61d319
Jonas Teuwen <j.teuwen@nki.nl>
2022-08-20 18:30:49 +0200

    Extend tests

--- tests/test_annotations.py
+++ tests/test_annotations.py
@@ -7,9 +7,10 @@ import json
 import pathlib
 import tempfile
 
+import numpy as np
 import pytest
 
-from dlup.annotations import WsiAnnotations
+from dlup.annotations import Polygon, WsiAnnotations
 
 ASAP_XML_EXAMPLE = b"""<?xml version="1.0"?>
 <ASAP_Annotations>
@@ -36,20 +37,52 @@ ASAP_XML_EXAMPLE = b"""<?xml version="1.0"?>
 </ASAP_Annotations>"""
 
 
-@pytest.mark.parametrize("scaling", [1, 5.0])
-# @pytest.mark.parametrize("split_per_label", [True, False])
-def test_annotations(scaling, split_per_label=False):
+class TestAnnotations:
     with tempfile.NamedTemporaryFile(suffix=".xml") as asap_file:
         asap_file.write(ASAP_XML_EXAMPLE)
         asap_file.flush()
-        asap_annotations = WsiAnnotations.from_asap_xml(pathlib.Path(asap_file.name), scaling=scaling)
+        annotations = WsiAnnotations.from_asap_xml(pathlib.Path(asap_file.name))
 
     with tempfile.NamedTemporaryFile(suffix=".json") as geojson_out:
-        asap_geojson = asap_annotations.as_geojson(split_per_label=split_per_label)
+        asap_geojson = annotations.as_geojson(split_per_label=False)
         geojson_out.write(json.dumps(asap_geojson).encode("utf-8"))
         geojson_out.flush()
 
         geojson_annotations = WsiAnnotations.from_geojson([pathlib.Path(geojson_out.name)], scaling=1)
-        geojson_geojson = geojson_annotations.as_geojson(split_per_label=split_per_label)
 
+    def test_asap_to_geojson(self, split_per_label=False):
+        asap_geojson = self.annotations.as_geojson(split_per_label=split_per_label)
+        geojson_geojson = self.geojson_annotations.as_geojson(split_per_label=split_per_label)
         assert asap_geojson == geojson_geojson
+
+    @pytest.mark.parametrize("region", [((10000, 10000), (5000, 5000), 3756.0), ((0, 0), (5000, 5000), None)])
+    def test_read_region(self, region):
+        coordinates, size, area = region
+        region = self.annotations.read_region(coordinates, 1.0, size)
+        if area and area > 0:
+            assert len(region) == 1
+            assert region[0].area == area
+            assert region[0].label == "healthy glands"
+            assert isinstance(region[0], Polygon)
+
+        if not area:
+            assert region == []
+
+    @pytest.mark.parametrize("scaling", [1, 5.0])
+    def test_remap_and_scaling_label(self, scaling):
+        remap_labels = {"healthy glands": "new_label"}
+        coordinates, size, area = ((10000, 10000), (5000, 5000), 3756.0)
+        coordinates = (np.asarray(coordinates) * scaling).tolist()
+        size = (np.asarray(size) * scaling).tolist()
+        with tempfile.NamedTemporaryFile(suffix=".json") as geojson_out:
+            geojson_out.write(json.dumps(self.annotations.as_geojson()).encode("utf-8"))
+            geojson_out.flush()
+            annotations = WsiAnnotations.from_geojson(
+                [pathlib.Path(geojson_out.name)], remap_labels=remap_labels, scaling=scaling
+            )
+
+        region = annotations.read_region(coordinates, 1.0, size)
+        assert len(region) == 1
+        assert region[0].area == scaling ** 2 * area
+        assert region[0].label == "new_label"
+        assert isinstance(region[0], Polygon)
