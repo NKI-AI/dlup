022e96fdc771856d6f92157b429ee1ec23751977
Ajey Pai K <ajeypie@gmail.com>
2022-09-15 14:21:05 +0200

    testing mpp of tiff images. Passes.

--- tests/test_writers.py
+++ tests/test_writers.py
@@ -9,10 +9,21 @@ import pyvips
 
 from dlup.utils.pyvips import vips_to_numpy
 from dlup.writers import TiffCompression, TifffileImageWriter
+from dlup import SlideImage
 
 
-@pytest.mark.parametrize("shape", [[1200, 1200], [800, 550, 3]])
-def test_tiff_writer(shape):
+@pytest.mark.parametrize(
+    ["shape", "target_mpp"],
+    [
+        [(1024, 1024), 0.45],
+        [(512, 512), 4],
+        [(2048, 2048), 0.25],
+        [(1024, 1024), 10],
+        [(512, 512), 0.8],
+        [(2048, 2048), 0.7],
+    ],
+)
+def test_tiff_writer(shape, target_mpp):
     random_array = np.random.randint(low=0, high=255, size=(1200, 1200, 3), dtype=np.uint8)
     pil_image = PIL.Image.fromarray(random_array)
 
@@ -22,8 +33,14 @@ def test_tiff_writer(shape):
         size = (*pil_image.size, 3)
 
     with tempfile.NamedTemporaryFile(suffix=".tiff") as temp_tiff:
-        writer = TifffileImageWriter(temp_tiff.name, size=size, mpp=(0.45, 0.45), compression=TiffCompression.NONE)
+        writer = TifffileImageWriter(
+            temp_tiff.name, size=size, mpp=(target_mpp, target_mpp), compression=TiffCompression.NONE
+        )
 
         writer.from_pil(pil_image)
         vips_image = pyvips.Image.new_from_file(temp_tiff.name)
         assert np.allclose(np.asarray(pil_image), vips_to_numpy(vips_image))
+
+        with SlideImage.from_file_path(temp_tiff.name) as slide:
+            slide_mpp = slide.mpp
+            assert np.allclose(slide_mpp, target_mpp)
