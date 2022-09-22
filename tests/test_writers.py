69388d796136fcbedb5ec9f230b768d02683edc8
Jonas Teuwen <j.teuwen@nki.nl>
2022-09-22 13:39:19 +0100

    Fix tiff test

--- tests/test_writers.py
+++ tests/test_writers.py
@@ -16,18 +16,19 @@ from dlup.writers import TiffCompression, TifffileImageWriter
     ["shape", "target_mpp"],
     [
         [(1024, 1024, 3), 0.45],
-        [(512, 512, 3), 4],
-        [(2048, 2048, 3), 0.25],
+        [(512, 768, 3), 4],
+        [(1024, 512, 3), 0.25],
         [(1024, 1024), 10],
-        [(512, 512), 0.8],
-        [(2048, 2048), 0.7],
+        [(512, 768), 0.8],
+        [(1024, 512), 0.7],
     ],
 )
 def test_tiff_writer(shape, target_mpp):
     random_array = np.random.randint(low=0, high=255, size=shape, dtype=np.uint8)
-    pil_image = PIL.Image.fromarray(random_array)
+    pil_image = PIL.Image.fromarray(random_array, mode="RGB" if len(shape) == 3 else "L")
+    mode = pil_image.mode
 
-    if pil_image.mode == "L":
+    if mode == "L":
         size = pil_image.size
     else:
         size = (*pil_image.size, 3)
@@ -39,7 +40,11 @@ def test_tiff_writer(shape, target_mpp):
 
         writer.from_pil(pil_image)
         vips_image = pyvips.Image.new_from_file(temp_tiff.name)
-        assert np.allclose(np.asarray(pil_image), vips_to_numpy(vips_image))
+        vips_image_numpy = vips_to_numpy(vips_image)
+        if mode == "L":
+            vips_image_numpy = vips_image_numpy[..., 0]
+
+        assert np.allclose(np.asarray(pil_image), vips_image_numpy)
 
         with SlideImage.from_file_path(temp_tiff.name) as slide:
             slide_mpp = slide.mpp
