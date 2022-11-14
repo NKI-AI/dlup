b76e833edd8ae71289100dc8dcf7d0e64a15a16a
Jonas Teuwen <j.teuwen@nki.nl>
2022-11-14 15:23:43 +0100

    Bump version: 0.3.8-dev0 → 0.3.9-dev0

--- dlup/__init__.py
+++ dlup/__init__.py
@@ -8,6 +8,6 @@ from ._region import BoundaryMode, RegionView
 
 __author__ = """dlup contributors"""
 __email__ = "j.teuwen@nki.nl"
-__version__ = "0.3.8-dev0"
+__version__ = "0.3.9-dev0"
 
 __all__ = ("SlideImage", "RegionView", "UnsupportedSlideError", "BoundaryMode")
