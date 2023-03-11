0216829116c531380e5c722ea83c07ee662fbab0
Jonas Teuwen <j.teuwen@nki.nl>
2023-03-11 15:56:23 +0100

    Bump version: 0.3.23-dev0 → 0.3.23

--- dlup/__init__.py
+++ dlup/__init__.py
@@ -8,6 +8,6 @@ from ._region import BoundaryMode, RegionView
 
 __author__ = """dlup contributors"""
 __email__ = "j.teuwen@nki.nl"
-__version__ = "0.3.23-dev0"
+__version__ = "0.3.23"
 
 __all__ = ("SlideImage", "RegionView", "UnsupportedSlideError", "BoundaryMode")
