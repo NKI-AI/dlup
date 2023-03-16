9a21ca50ff0a26bc36ef4a1fe7d4d5df5fee3a67
Siem de Jong <28396796+siemdejong@users.noreply.github.com>
2023-03-16 19:29:30 +0100

    docs: fix typo

--- dlup/background.py
+++ dlup/background.py
@@ -188,7 +188,7 @@ def entropy_masker(
     ----------
     image : np.ndarray
         2D RGB image, with color channels at the last axis.
-    footprint : np.ndarray, default=`skimage.morphology.disk(3)`
+    footprint : np.ndarray, default=`skimage.morphology.disk(5)`
         Footprint to use with `skimage.filters.rank.entropy`.
     bins : int
         Number of bins for the entropy histogram.
