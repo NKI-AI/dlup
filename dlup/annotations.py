b7c2698894126369f35530bdb7d548f5858b21d2
Jonas Teuwen <jonasteuwen@gmail.com>
2024-08-17 14:06:37 +0200

    minor changes in annotations

--- dlup/annotations.py
+++ dlup/annotations.py
@@ -14,7 +14,7 @@ Supported file formats:
 - HaloXML
 """
 from __future__ import annotations
-import time
+
 import copy
 import errno
 import functools
@@ -923,6 +923,7 @@ class WsiAnnotations:
             one can annotate a larger region, and the smaller regions should overwrite the previous part.
             A function `dlup.data.transforms.convert_annotations` can be used to convert such outputs to a mask.
         3.  The annotations are cropped to the region-of-interest, or filtered in case of points. Polygons which
+            convert into points after intersection are removed. If it's a image-level label, nothing happens.
         4.  The annotation is rescaled and shifted to the origin to match the local patch coordinate system.
 
         The final returned data is a list of `dlup.annotations.Polygon` or `dlup.annotations.Point`.
@@ -980,6 +981,7 @@ class WsiAnnotations:
         for annotation in cropped_annotations:
             annotation = transform(annotation, _affine_coords)
             output.append(annotation)
+        return output
 
     def __str__(self) -> str:
         return (
