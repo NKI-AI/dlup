1311d553e71811af97bf4ec08ef038daad32a994
Jonas Teuwen <jonasteuwen@gmail.com>
2024-08-24 13:53:38 +0200

    Refactor

--- src/geometry/region.h
+++ src/geometry/region.h
@@ -13,77 +13,69 @@ class Polygon;
 class Box;
 class Point;
 
+template <typename T>
+class AnnotationRegionBase {
+  public:
+  AnnotationRegionBase(std::vector<std::shared_ptr<T>> objects) : objects_(std::move(objects)) {}
+
+  // Factory function setter
+  static void setFactory(py::function factory) { FactoryManager<T>::setFactory(std::move(factory)); }
+
+  // FactoryGuard creator
+  static FactoryGuard createFactoryGuard(py::function factory) {
+    return FactoryManager<T>::createFactoryGuard(std::move(factory));
+  }
+
+  // Factory function caller
+  static py::object callFactoryFunction(const std::shared_ptr<T> &object) {
+    return FactoryManager<T>::callFactoryFunction(object);
+  }
+
+  std::vector<std::shared_ptr<T>> getObjectVector() const { return objects_; }
+
+  py::list getObjects() const {
+    py::list py_objects;
+    for (const auto &object : objects_) {
+      py_objects.append(callFactoryFunction(object));
+    }
+    return py_objects;
+  }
+
+  private:
+  std::vector<std::shared_ptr<T>> objects_;
+};
+
 class AnnotationRegion {
   public:
   AnnotationRegion(std::vector<std::shared_ptr<Polygon>> polygons, std::vector<std::shared_ptr<Box>> boxes,
                    std::vector<std::shared_ptr<Point>> points, std::tuple<int, int> mask_size)
-      : polygons_(std::move(polygons)), boxes_(std::move(boxes)), points_(std::move(points)),
+      : polygon_region_(std::move(polygons)), box_region_(std::move(boxes)), point_region_(std::move(points)),
         mask_size_(std::move(mask_size)) {}
 
-  // Factory function setters
-  static void setPolygonFactory(py::function factory) { FactoryManager<Polygon>::setFactory(std::move(factory)); }
-  static void setBoxFactory(py::function factory) { FactoryManager<Box>::setFactory(std::move(factory)); }
-  static void setPointFactory(py::function factory) { FactoryManager<Point>::setFactory(std::move(factory)); }
+  // Templated factory function setters
+  template <typename T>
+  static void setFactory(py::function factory) { AnnotationRegionBase<T>::setFactory(std::move(factory)); }
 
-  // FactoryGuard creators
-  static FactoryGuard createPolygonFactoryGuard(py::function factory) {
-    return FactoryManager<Polygon>::createFactoryGuard(std::move(factory));
-  }
-  static FactoryGuard createPointFactoryGuard(py::function factory) {
-    return FactoryManager<Point>::createFactoryGuard(std::move(factory));
-  }
-  static FactoryGuard createBoxFactoryGuard(py::function factory) {
-    return FactoryManager<Box>::createFactoryGuard(std::move(factory));
-  }
-
-  // Factory function callers
-  static py::object callPolygonFactory(const std::shared_ptr<Polygon> &polygon) {
-    return FactoryManager<Polygon>::callFactoryFunction(polygon);
-  }
-
-  static py::object callBoxFactory(const std::shared_ptr<Box> &box) {
-    return FactoryManager<Box>::callFactoryFunction(box);
-  }
-
-  static py::object callPointFactory(const std::shared_ptr<Point> &point) {
-    return FactoryManager<Point>::callFactoryFunction(point);
+  template <typename T>
+  static FactoryGuard createFactoryGuard(py::function factory) {
+    return AnnotationRegionBase<T>::createFactoryGuard(std::move(factory));
   }
 
   // Member functions to retrieve annotations
-  py::list getPolygons() const {
-    py::list py_polygons;
-    for (const auto &polygon : polygons_) {
-      py_polygons.append(callPolygonFactory(polygon));
-    }
-    return py_polygons;
-  }
-
-  py::list getPoints() const {
-    py::list py_points;
-    for (const auto &point : points_) {
-      py_points.append(callPointFactory(point));
-    }
-    return py_points;
-  }
-
-  py::list getBoxes() const {
-    py::list py_boxes;
-    for (const auto &box : boxes_) {
-      py_boxes.append(callBoxFactory(box));
-    }
-    return py_boxes;
-  }
+  py::list getPolygons() const { return polygon_region_.getObjects(); }
+  py::list getPoints() const { return point_region_.getObjects(); }
+  py::list getBoxes() const { return box_region_.getObjects(); }
 
   py::array_t<int> toMask(int default_value = 0) const {
     cv::Size region_size(std::get<0>(mask_size_), std::get<1>(mask_size_));
-    cv::Mat mask = generateMaskFromAnnotations(polygons_, region_size, default_value);
+    cv::Mat mask = generateMaskFromAnnotations(polygon_region_.getObjectVector(), region_size, default_value);
     return maskToPyArray(mask);
   }
 
   private:
-  std::vector<std::shared_ptr<Polygon>> polygons_;
-  std::vector<std::shared_ptr<Point>> points_;
-  std::vector<std::shared_ptr<Box>> boxes_;
+  AnnotationRegionBase<Polygon> polygon_region_;
+  AnnotationRegionBase<Point> point_region_;
+  AnnotationRegionBase<Box> box_region_;
   std::tuple<int, int> mask_size_;
 };
 
