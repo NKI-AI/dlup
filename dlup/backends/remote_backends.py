09f08a55120cadf7db1d028ab653ddfad725dc0e
Bart de Rooij <bpderooij@gmail.com>
2025-01-09 09:50:23 +0100

    Split deepzoom backend, simplify remote backend, small edge case fix

--- dlup/backends/remote_backends.py
+++ dlup/backends/remote_backends.py
@@ -1,26 +1,207 @@
 from abc import abstractmethod
-from typing import Any
+from io import BytesIO
+from pathlib import Path
+from typing import Any, Optional, Union
+from urllib.parse import ParseResult, urlparse
 
 from dlup._types import PathLike
 from dlup.backends.common import AbstractSlideBackend
+from dlup.utils.imports import AIOHTTP_AVAILABLE
+
+if AIOHTTP_AVAILABLE:
+    import asyncio
+
+    import aiohttp
+
+
+DEFAULT_MAX_ASYNC_REQUESTS = 6  # This is the number of requests to make asynchronously
 
 
 class RemoteSlideBackend(AbstractSlideBackend):
+    """
+    Abstract base class for remote slide experimental_backends
+    """
+
+    _max_async_request = DEFAULT_MAX_ASYNC_REQUESTS
+
     def __init__(self, filename: PathLike) -> None:
+        """
+        Parameters
+        ----------
+        filename : PathLike
+            `PathLike` object representing the URL of the remote slide.
+
+        Raises
+        ------
+        ValueError
+            If filename is not a `str`, but a `Path` object.
+        RuntimeError
+            If `aiohttp` is not available.
+        """
+        if not AIOHTTP_AVAILABLE:
+            raise RuntimeError("`aiohttp` is not available. Install dlup with `remote_backends` dependencies.")
+
+        if isinstance(filename, Path):
+            raise ValueError("Filename should be URL string for remote slides.")
+
+        # Some parts of the URL are used frequently, and can be retrieved as properties
+        self._parsed_url: ParseResult = urlparse(filename)
+        self._base_url: str = f"{self.scheme}://{self.netloc}"  # Combine scheme and netloc for efficient use
+
+        self.cookies: Optional[dict[str, str]] = None
+        self.headers: Optional[dict[str, str]] = None
         self._set_metadata()
         super().__init__(filename)
 
     @property
     def properties(self) -> dict[str, Any]:
+        """Properties of the slide. This can be expensive to fetch, so it is set with `self._fetch_properties`."""
         if not hasattr(self, "_properties"):
             self._properties = self._fetch_properties()
         return self._properties
 
+    @property
+    def scheme(self) -> str:
+        """
+        Retrieve the scheme (e.g., 'http', 'https') of the parsed URL.
+        """
+        return self._parsed_url.scheme
+
+    @property
+    def netloc(self) -> str:
+        """
+        Retrieve the network location (e.g., 'example.com:80', '') of the parsed URL.
+        """
+        return self._parsed_url.netloc
+
+    @property
+    def base_url(self) -> str:
+        """
+        Retrieve the base url (e.g., 'https://example.com:80') consisting of the combined scheme and netloc of the
+        parsed URL.
+        """
+        return self._base_url
+
+    @property
+    def path(self) -> str:
+        """
+        Retrieve the path (e.g., '/path/to/resource', '') of the parsed URL.
+        """
+        return self._parsed_url.path
+
+    @property
+    def query(self) -> str:
+        """
+        Retrieve the query string (e.g., 'key=value', '') of the parsed URL.
+        """
+        return self._parsed_url.query
+
+    @property
+    def hostname(self) -> Optional[str]:
+        """
+        Retrieve the hostname (e.g., 'example.com') of the parsed URL.
+        """
+        return self._parsed_url.hostname
+
+    @property
+    def port(self) -> Optional[int]:
+        """
+        Retrieve the port (e.g., 80, 443) of the parsed URL.
+        """
+        return self._parsed_url.port
+
     @abstractmethod
     def _fetch_properties(self) -> dict[str, Any]:
+        """Fetch slide properties from the remote server"""
         pass
 
     @abstractmethod
     def _set_metadata(self) -> None:
         """Metadata needed for remote access"""
         pass
+
+    async def _fetch_single(
+        self,
+        session: aiohttp.ClientSession,  # pylint: disable=possibly-used-before-assignment
+        url: str,
+        data: Optional[dict[str, Any]],
+    ) -> BytesIO:
+        """Perform a HTTP get request to the url (with extra data)
+
+        Parameters
+        ----------
+        session : aiohttp.ClientSession
+            Interface for making the HTTP requests
+        url : str
+            URL to perform get request
+        data : Optional[dict[str, Any]]
+            Additional data for the get request, by default None.
+
+        Returns
+        -------
+        BytesIO
+            Server response as a BytesIO object if status == 200 (succes), raises response status otherwise.
+        """
+        async with session.get(url, data=data) as response:
+            response.raise_for_status()
+            byte_data = await response.read()
+            return BytesIO(byte_data)
+
+    async def _fetch_multiple(
+        self,
+        urls: list[str],
+        data_list: Optional[list[Optional[dict[str, Any]]]] = None,
+    ) -> list[BytesIO]:
+        """Asychronously fetch a list of requests with optional extra data.
+
+        Parameters
+        ----------
+        urls : list[str]
+            A list of URLs to fetch.
+        data_list : Optional[list[Optional[dict[str, Any]]]], optional
+            A list of dictionaries containing data for each URL, or None for no data.
+
+        Returns
+        -------
+        list[BytesIO]
+            List of server responses as BytesIO objects.
+        """
+        if data_list is None:
+            data_list = [None] * len(urls)
+
+        if len(urls) != len(data_list):
+            raise ValueError("Number of URLs and data dictionaries should be the same.")
+
+        connector = aiohttp.TCPConnector(limit=self._max_async_request)
+        async with aiohttp.ClientSession(cookies=self.cookies, headers=self.headers, connector=connector) as session:
+            tasks = [self._fetch_single(session=session, url=url, data=data) for url, data in zip(urls, data_list)]
+            return await asyncio.gather(*tasks)  # pylint: disable=possibly-used-before-assignment
+
+    def fetch(
+        self,
+        urls: Union[str, list[str]],
+        data: Optional[Union[dict[str, Any], list[Optional[dict[str, Any]]]]] = None,
+    ) -> Union[BytesIO, list[BytesIO]]:
+        """
+        Fetch data from one or more URLs synchronously.
+
+        This method wraps the asynchronous fetching logic in a synchronous interface.
+
+        Parameters
+        ----------
+        urls : Union[str, list[str]]
+            A single URL or a list of URLs to fetch.
+        data : Optional[Union[dict[str, Any], list[Optional[dict[str, Any]]]]]
+            A dictionary of data for a single request, or a list of dictionaries for multiple requests.
+
+        Returns
+        -------
+        Union[BytesIO, List[BytesIO]]
+            A BytesIO object for a single URL or a list of BytesIO objects for multiple URLs.
+        """
+        if isinstance(urls, str) and (isinstance(data, dict) or data is None):
+            return asyncio.run(self._fetch_multiple([urls], [data]))[0]
+        elif isinstance(urls, list) and (isinstance(data, list) or data is None):
+            return asyncio.run(self._fetch_multiple(urls, data))
+        else:
+            raise ValueError(f"URLs and data should be either both strings or both lists, got {urls=} and {data=}.")
--- dlup/backends/slidescore_backend.py
+++ dlup/backends/slidescore_backend.py
@@ -3,25 +3,18 @@ from __future__ import annotations
 import functools
 import json
 import os
-import pathlib
 import re
 from io import BytesIO
-from typing import Any, Optional
+from typing import Any
+
+import pyvips
 
 from dlup._types import PathLike
-from dlup.backends.deepzoom_backend import DeepZoomSlide, TileResponseTypes
+from dlup.backends.deepzoom_backend import AbstractDeepZoomSlide
 from dlup.backends.remote_backends import RemoteSlideBackend
 from dlup.utils.backends import dict_to_snake_case, parse_xml_to_dict
-from dlup.utils.imports import AIOHTTP_AVAILABLE
-
-if AIOHTTP_AVAILABLE:
-    import asyncio
-
-    import aiohttp
-
 
 METADATA_CACHE = 128
-DEFAULT_ASYNC_REQUESTS = 6  # This is the number of requests to make asynchronously
 API_TOKEN_OS_VARIABLE_NAME = "SLIDESCORE_API_TOKEN"
 
 
@@ -37,54 +30,27 @@ def open_slide(filename: PathLike) -> "SlideScoreSlide":
     return SlideScoreSlide(filename)
 
 
-class SlideScoreSlide(RemoteSlideBackend, DeepZoomSlide):
-    _max_async_request = DEFAULT_ASYNC_REQUESTS
-
+class SlideScoreSlide(RemoteSlideBackend, AbstractDeepZoomSlide):
     def __init__(self, filename: PathLike):
-        if isinstance(filename, pathlib.Path):
-            raise ValueError("Filename should be SlideScore URL for SlideScoreSlide.")
-
-        if not AIOHTTP_AVAILABLE:
-            raise RuntimeError("`aiohttp` is not available. Install dlup with `slidescore_remote` dependencies.")
-
-        # Parse URL with regex
-        parsed_url = re.search(r"(https?://[^/?]+)(?=.*\bstudyId=(\d+))(?=.*\bimageId=(\d+)).*$", filename)
-        if parsed_url is None:
-            raise ValueError("Could not parse URL into valid SlideScore parameters.")
-        server_url, study_id, image_id = parsed_url.groups()
-        self._server_url = server_url
-        self._study_id = int(study_id)
-        self._image_id = int(image_id)
-        self.cookies: Optional[dict[str, str]] = None
-        self.headers: Optional[dict[str, str]] = None
-        # MRO will take care of setting up metadata: RemoteSlide -> DeepZoomSlide -> AbstractSlide
+        # MRO will take care of setting up metadata. The diamond inheritance is a bit wonky here, but it works.
+        # Validation of slidedetails and tileserver properties is done in the set_metadata method
         super().__init__(filename)
 
     @functools.lru_cache(maxsize=METADATA_CACHE)
     def _fetch_properties(self) -> dict[str, Any]:
         """Fetch properties from GetImageMetadata SlideScore API endpoint"""
-        response = self.run_fetch_requests(
-            urls=[f"{self._server_url}/Api/GetImageMetadata"], data_dicts=[{"imageid": self._image_id}]
-        )[0]
+        response = self.fetch(urls=f"{self.base_url}/Api/GetImageMetadata", data={"imageid": self._image_id})
+        assert isinstance(response, BytesIO)  # Fetch should return a single BytesIO object here
         metadata: dict[str, Any] = json.load(response)["metadata"]
         return dict_to_snake_case(metadata)
 
     @functools.lru_cache(maxsize=METADATA_CACHE)
     def _fetch_dz_properties(self) -> dict[str, Any]:
         """Fetch deepzoom properties from GetTileServer SlideScore API endpoint and parse XML resonse"""
-        dz_properties = {}
-        tile_server_response = self.run_fetch_requests(
-            urls=[f"{self._server_url}/Api/GetTileServer"], data_dicts=[{"imageid": self._image_id}]
-        )[0]
-        # Contains JSON object with cookiePart, urlPart (to be used in the calls to /i/ endpoint) and expiresOn
-        tile_server: dict[str, Any] = json.load(tile_server_response)
-        dz_properties.update(dict_to_snake_case(tile_server))
-
-        # Set cookies here to make deepzoom request
-        self.cookies = {"t": dz_properties["cookie_part"]}
-        dzi_url = f"{self._server_url}/i/{self._image_id}/{dz_properties['url_part']}/i.dzi"
-        dzi_response = self.run_fetch_requests(urls=[dzi_url])[0]
-        dz_properties.update(parse_xml_to_dict(dzi_response))
+        dzi_url = f"{self.base_url}/i/{self._image_id}/{self.tile_server_properties['url_part']}/i.dzi"
+        dzi_response = self.fetch(urls=dzi_url)
+        assert isinstance(dzi_response, BytesIO)  # Fetch will return a single BytesIO object here
+        dz_properties = parse_xml_to_dict(dzi_response)
         return dz_properties
 
     @property
@@ -93,9 +59,9 @@ class SlideScoreSlide(RemoteSlideBackend, DeepZoomSlide):
         return "SlideScore"
 
     @property
-    def tile_files(self) -> str:
+    def tile_files_root(self) -> str:
         """Returns the SlideScore API endpoint where deep zoom tiles are stored."""
-        return f"{self._server_url}/i/{self._image_id}/{self.dz_properties['url_part']}/i_files"
+        return f"{self.base_url}/i/{self._image_id}/{self.tile_server_properties['url_part']}/i_files"
 
     def _set_metadata(self) -> None:
         """Set up metadata for SlideScore server requests and image metadata.
@@ -112,99 +78,31 @@ class SlideScoreSlide(RemoteSlideBackend, DeepZoomSlide):
         if api_token is None:
             raise RuntimeError("SlideScore API token not found. Please set SLIDESCORE_API_TOKEN in os environment")
         self.headers = {"Accept": "application/json", "Authorization": f"Bearer {api_token}"}
-        self.cookies = {"t": self.dz_properties["cookie_part"]}
 
-        # Sanity check for study_id
-        response = self.run_fetch_requests(
-            urls=[f"{self._server_url}/Api/GetSlideDetails"], data_dicts=[{"imageid": self._image_id}]
-        )[0]
-        slide_details = json.load(response)
-        if self._study_id != int(slide_details["studyID"]):
-            raise RuntimeError(
-                f"Slidescore Study ID does not correspond got {slide_details['studyID']} but expected {self._study_id}"
-            )
+        # Look for studyId and imageId in the URL query (not case sensitive)
+        query_match = re.search(r"(?=.*\bstudyId=(\d+))(?=.*\bimageId=(\d+)).*$", self.query, re.IGNORECASE)
+        if query_match is None:
+            raise ValueError("Could not parse URL into valid SlideScore studyId and imageId.")
+        study_id, image_id = query_match.groups()
+        self._study_id = int(study_id)
+        self._image_id = int(image_id)
 
-    async def fetch_request(
-        self,
-        session: aiohttp.ClientSession,
-        url: str,
-        data: Optional[dict[str, Any]],
-    ) -> BytesIO:
-        """Perform a HTTP get request to the url (with extra data)
+        tile_server_response = self.fetch(urls=f"{self.base_url}/Api/GetTileServer", data={"imageid": self._image_id})
+        assert isinstance(tile_server_response, BytesIO)  # Fetch should return a single BytesIO object here
 
-        Parameters
-        ----------
-        session : aiohttp.ClientSession
-            Interface for making the HTTP requests
-        url : str
-            URL to perform get request
-        data : Optional[dict[str, Any]]
-            Additional data for the get request.
+        # Contains JSON object with cookiePart, urlPart (to be used in the calls to /i/ endpoint) and expiresOn
+        self.tile_server_properties: dict[str, Any] = dict_to_snake_case(json.load(tile_server_response))
+        self.cookies = {"t": self.tile_server_properties["cookie_part"]}
 
-        Returns
-        -------
-        BytesIO
-            Server response as a BytesIO object if status == 200 (succes), raises response status otherwise.
-        """
-        async with session.get(url, data=data) as response:
-            if response.status == 200:
-                byte_data = await response.read()
-                return BytesIO(byte_data)
-            else:
-                response.raise_for_status()
-                raise RuntimeError("Failed to get response from server")  # Raise error for mypy
+        # Validate that we are looking at the correct slide by checking the studyID
+        response = self.fetch(urls=f"{self.base_url}/Api/GetSlideDetails", data={"imageid": self._image_id})
+        assert (
+            isinstance(response, BytesIO) and json.load(response)["studyID"] == self._study_id
+        ), "Slidescore Study ID does not correspond to the slide."
 
-    async def fetch_requests(
-        self,
-        urls: list[str],
-        data_dicts: Optional[list[dict[str, Any] | None]] = None,
-    ) -> list[BytesIO]:
-        """Asychronously fetch a list of requests with optional extra data.
-
-        Parameters
-        ----------
-        urls : list[str]
-            List of (base) URLs to get response.
-        data_dicts : Optional[list[dict[str, Any]]], optional
-            Optionally, add dictionary with data per URL, by default None
-
-        Returns
-        -------
-        list[Any]
-            List of server responses as BytesIO objects.
-        """
-        if data_dicts is None:
-            data_dicts = [None] * len(urls)
-
-        connector = aiohttp.TCPConnector(limit=self._max_async_request)
-        async with aiohttp.ClientSession(cookies=self.cookies, headers=self.headers, connector=connector) as session:
-            tasks = [self.fetch_request(session=session, url=url, data=data) for url, data in zip(urls, data_dicts)]
-            return await asyncio.gather(*tasks)  # pylint: disable=possibly-used-before-assignment
-
-    def run_fetch_requests(
-        self,
-        urls: list[str],
-        data_dicts: Optional[list[dict[str, Any] | None]] = None,
-    ) -> list[Any]:
-        """Collect and return all asychronously fetched requests.
-
-        Parameters
-        ----------
-        urls : list[str]
-            List of (base) URLs to get response.
-        data_dicts : Optional[list[dict[str, Any] | None]], optional
-            Optionally, add dictionary with data per URL, by default None
-
-        Returns
-        -------
-        list[BytesIO]
-            List of server responses as BytesIO objects.
-        """
-        return asyncio.run(self.fetch_requests(urls, data_dicts=data_dicts))
-
-    def retrieve_deepzoom_tiles(self, level: int, indices: list[tuple[int, int]]) -> list[TileResponseTypes]:
-        """Retrieve ByteIO objects for tile indices of deepzoom level. These tiles will be opened with Pillow
-        and stitched together in `read_region`. The server requests are made asynchronously.
+    def _fetch_deepzoom_tile_files(self, level: int, indices: list[tuple[int, int]]) -> list[BytesIO]:
+        """Retrieve ByteIO objects for tile indices of deepzoom level. The image data will be fetched from the
+        SlideScore server in a buffer. The tiles will be fetched asynchronously and stitched together in `read_region`.
 
         Parameters
         ----------
@@ -219,16 +117,18 @@ class SlideScoreSlide(RemoteSlideBackend, DeepZoomSlide):
             List of ByteIO objects for unprocessed DeepZoom tiles.
         """
         # Super will return list of urls as string
-        tile_urls: list[str] = super().retrieve_deepzoom_tiles(level, indices)  # type: ignore
-        tile_responses = self.run_fetch_requests(tile_urls)
+        tile_files_root, file_format = self.tile_files_root, self.dz_properties["image"]["format"]
+        tile_urls: list[str] = [f"{tile_files_root}/{level}/{col}_{row}.{file_format}" for row, col in indices]
+        tile_responses = self.fetch(tile_urls)
+        assert isinstance(tile_responses, list)  # Fetch will return a list of BytesIO objects here
         return tile_responses
+
+    def _open_deepzoom_tile(self, filename: BytesIO) -> pyvips.Image:
+        return pyvips.Image.new_from_buffer(filename.getvalue(), "")
 
     def close(self) -> None:
         """Close the underlying slide"""
-        del self.headers
-        del self.cookies
-        del self._dz_properties
-        return
+        pass
 
 
 def export_api_key(file_path: PathLike, os_variable_name: str = API_TOKEN_OS_VARIABLE_NAME) -> None:
